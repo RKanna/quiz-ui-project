e0391465a19e6c1bf3635beaf1604113f58b620a
RKanna <rkannan.balakrishnan@hotmail.com>
2023-07-09 03:14:50 +0530

    Script work fully done

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 "use strict";
 let checkboxes = document.getElementsByName("option");
 let submitButton = document.getElementById("submitbtn");
+let Reloadbtn = document.getElementById("Reloadbtn");
 let questionContainer = document.querySelectorAll(
   ".question-two-container, .question-three-container, .question-four-container, .result-container"
 );
@@ -9,6 +10,16 @@ let questionTwoContainer = document.getElementById("questionTwo-Container");
 let questionThreeContainer = document.getElementById("questionThree-Container");
 let questionFourContainer = document.getElementById("questionFour-Container");
 let resultContainer = document.getElementById("result-container");
+let finalResult = document.getElementById("finalResult");
+let removeBtnContainer = document.getElementById("btn-container-remove");
+let addBtnContainer = document.getElementById("btnAdd");
+
+//answers
+
+let javascriptAnswer = document.getElementById("javascriptAnswer");
+let cascadingAnswer = document.getElementById("cascadingAnswer");
+let hyperTextAnswer = document.getElementById("hyperTextAnswer");
+let yearAnswer = document.getElementById("yearAnswer");
 
 questionOneContainer.style.display = "block";
 questionTwoContainer.style.display = "none";
@@ -25,17 +36,6 @@ function toggleCheckbox(checkbox) {
     }
   }
 }
-
-// submitButton.addEventListener("click", function () {
-//   for (var i = 0; i < checkboxes.length; i++) {
-//     console.log(checkboxes[i].checked);
-//   }
-
-//   if ((questionOneContainer.style.display = "block" && checkboxes[i].checked)) {
-//     questionOneContainer.style.display = "none";
-//     questionTwoContainer.style.display = "block";
-//   }
-// });
 
 submitButton.addEventListener("click", function () {
   for (var i = 0; i < checkboxes.length; i++) {
@@ -64,6 +64,95 @@ submitButton.addEventListener("click", function () {
     ) {
       questionFourContainer.style.display = "none";
       resultContainer.style.display = "flex";
+      submitButton.classList.add("btnReload");
+      Reloadbtn.classList.remove("btnReload");
     }
   }
 });
+
+let score = 0;
+function checkToggle() {
+  let javascriptAnswer = document.getElementById("javascriptAnswer");
+  let cascadingAnswer = document.getElementById("cascadingAnswer");
+  let hyperTextAnswer = document.getElementById("hyperTextAnswer");
+  let yearAnswer = document.getElementById("yearAnswer");
+
+  if (javascriptAnswer.checked) {
+    score = score + 1;
+  }
+
+  if (cascadingAnswer.checked) {
+    score = score + 1;
+  }
+
+  if (hyperTextAnswer.checked) {
+    score = score + 1;
+  }
+
+  if (yearAnswer.checked) {
+    score = score + 1;
+  }
+
+  console.log("Score:", score);
+  finalResult.innerText = `You answered ${score}/4 questions correctly`;
+}
+
+function reloadFunction() {
+  location.reload();
+}
+
+//inside submitbtn Event
+
+// let score = 0;
+// checkboxes[0].checked = true;
+// console.log(checkboxes[3]);
+// if(javascriptAnswer.value == 4){
+
+// }
+
+// else if ((resultContainer.style.display = "flex")) {
+//   resultContainer.style.display = "flex";
+//   // submitButton.classList.add("btnReload");
+//   // Reloadbtn.classList.remove("btnReload");
+//   removeBtnContainer.style.display = "none";
+//   addBtnContainer.style.display = "block";
+// }
+
+//individual function
+
+// function checkToggle() {
+//   let score = 0;
+//   let javascriptAnswer = document.getElementById("javascriptAnswer");
+//   let cascadingAnswer = document.getElementById("cascadingAnswer");
+//   let hyperTextAnswer = document.getElementById("hyperTextAnswer");
+//   let yearAnswer = document.getElementById("yearAnswer");
+
+//   if (javascriptAnswer.checked) {
+//     console.log("Checkbox is toggled ON");
+//     score = score + 1;
+
+//   } else if (cascadingAnswer.checked) {
+//     console.log("Checkbox is toggled ON");
+//     score = score + 1;
+
+//   } else if (hyperTextAnswer.checked) {
+//     console.log("Checkbox is toggled ON");
+//     score = score + 1;
+//   } else if (yearAnswer.checked) {
+//     console.log("Checkbox is toggled ON");
+//     score = score + 1;
+//     console.log(score);
+//   } else {
+//   }
+// }
+
+// submitButton.addEventListener("click", function () {
+//   for (var i = 0; i < checkboxes.length; i++) {
+//     console.log(checkboxes[i].checked);
+//   }
+
+//   if ((questionOneContainer.style.display = "block" && checkboxes[i].checked)) {
+//     questionOneContainer.style.display = "none";
+//     questionTwoContainer.style.display = "block";
+//   }
+// });
